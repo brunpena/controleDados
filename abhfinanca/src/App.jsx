f9b30332ee364b14def07429ddb0d99557e36cb7
gimenezhiago <163415252+gimenezhiago@users.noreply.github.com>
2025-06-23 14:23:39 -0300

    rthyjk

---
+++ abhfinanca/src/App.jsx
@@ -0,0 +1,16 @@
+import * as C from './App.styles.js'
+
+const App = () => {
+  return (
+    <C.Container>
+      <C.Header>
+        <C.HeaderText>ABH - Finanças</C.HeaderText>
+      </C.Header>
+      <C.Body>
+        
+      </C.Body>
+    </C.Container>
+  )
+}
+
+export default App
