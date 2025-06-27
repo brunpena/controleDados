1d269c860a4df9372edf3dcea02faac1ab8f5611
gimenezhiago <163415252+gimenezhiago@users.noreply.github.com>
2025-06-26 23:40:56 -0300

    xzfgh

--- abhfinanca/src/data/itens.js
+++ abhfinanca/src/data/itens.js
@@ -1,38 +1,3 @@
 export const itens = [
-    { 
-        date: new Date(2025, 0, 1),
-        category: 'food',
-        title: 'Supermercado',
-        value: 150.00
-    },
-    {
-        date: new Date(2025, 5, 2),
-        category: 'transport',
-        title: 'Combustível',
-        value: 80.00
-    },
-    {
-        date: new Date(2025, 3, 3),
-        category: 'health',
-        title: 'Farmácia',
-        value: 45.00
-    },
-    {
-        date: new Date(2025, 5, 4),
-        category: 'leisure',
-        title: 'Cinema',
-        value: 30.50
-    },
-    {
-        date: new Date(2025, 4, 5),
-        category: 'rent',
-        title: 'Aluguel',
-        value: 1200.00
-    },
-    {
-        date: new Date(2025, 6, 6),
-        category: 'salary',
-        title: 'Salário',
-        value: 3000.00
-    }
+    
 ]
